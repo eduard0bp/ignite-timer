702f5642e0c73c51456aec0917af5f46ea380c36
Eduardo Batista Pereira <79476975+eduard0bp@users.noreply.github.com>
2023-03-08 17:37:41 -0300

    add: adicionando rotas e organizando pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="sucess" />
-      <Button />
-
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+
   * {
     margin: 0;
     padding: 0;
@@ -8,15 +9,16 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   :focus {
-    outline: 0;
-    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']}
+    outline: none;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
   }
 
   body {
-    background: ${(props) => props.theme['gray-900']}};
+    background: ${(props) => props.theme['gray-900']};
     color: ${(props) => props.theme['gray-300']};
+  }
 
-  body, input, textarea {
+  body, input, textarea, button {
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
     font-size: 1rem;
